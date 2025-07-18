c33e1a3f3fe163de0f4666b71aca384529abfcea
Gaurav Tripathi <gaurav.tripathi@timesinternet.in>
2025-07-19 01:12:55 +0530

    promise.race and promise.any

--- js/promise.any.js
+++ js/promise.any.js
@@ -1,2 +1,60 @@
 
 // polyfill of promise.any
+
+// It is quite similar to Promise.race() but it will wait and retuens 1st success promise
+
+const promiseAny = (promises) => {
+    return new Promise((resolve, reject) => {
+        if(!Array.isArray(promises)){
+            return reject('Must be an array type');
+        }
+
+        const rejections = [];
+        let rejectionCount = 0;
+        const total = promises.length;
+
+        if(total === 0){
+            return reject(new AggregateError([], 'All promises were rejected'));
+        }
+
+        promises.forEach((promise, index) => {
+            Promise.resolve(promise)
+            .then(resolve)
+            .catch((err) => {
+                rejections[index] = err;
+                rejectionCount += 1;
+
+                if(rejectionCount === total){
+                    return reject(new AggregateError([], 'All promises were rejected'));
+                }
+            })
+        })
+    })
+}
+
+const p8 = Promise.reject('any1');
+const p9 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('any2');
+    }, 1);
+})
+const p10 = Promise.resolve('any3');
+
+
+promiseAny([p8, p9, p10])
+.then((value) => {
+    console.log(value);
+})
+.catch((err) => {
+    console('caught: '+err);
+})
+
+// In Built method
+
+Promise.any([p8, p9, p10])
+.then((value) => {
+    console.log(value)
+})
+.catch((err) => {
+    console.log('Caught: '+ err);
+})
--- js/promise.race.js
+++ js/promise.race.js
@@ -1,11 +1,29 @@
 
 // Polyfill for promice.race
 
-const promiseRace = (promises) => {
 
+const promiseRace = (promises) => {
+    return new Promise((resolve, reject) => {
+        if(!Array.isArray(promises)){
+            return reject('Must be an array type - Race')
+        }
+
+        if(promises.length === 0){
+            return resolve([]);
+        }
+        for(const promise of promises){
+            Promise.resolve(promise)
+            .then(resolve)
+            .catch(reject)
+        }
+    })
 }
 
-const p5 = Promise.resolve('race1');
+const p5 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('race1');
+    }, 0);
+})
 const p6 = Promise.resolve('race2');
 const p7 = Promise.resolve('race3');
 
@@ -26,3 +44,5 @@ Promise.race([p5, p6, p7])
 .catch((err) => {
     console.log('Caught: '+err);
 })
+
+// It takes an array of promises and returns the first settled promise wheather it is success or failure.
