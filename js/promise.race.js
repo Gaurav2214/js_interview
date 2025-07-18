22b99308a5a75c65f3b7e1bd60f47e0a129ed1e3
Gaurav Tripathi <gaurav.tripathi@timesinternet.in>
2025-07-19 00:11:22 +0530

    changes

--- js/promise.race.js
+++ js/promise.race.js
@@ -1,2 +1,28 @@
 
 // Polyfill for promice.race
+
+const promiseRace = (promises) => {
+
+}
+
+const p5 = Promise.resolve('race1');
+const p6 = Promise.resolve('race2');
+const p7 = Promise.resolve('race3');
+
+promiseRace([p5, p6, p7])
+.then((value) => {
+    console.log(value);
+})
+.catch((err) => {
+    console.log('Caught: '+ err);
+});
+
+// In Built method
+
+Promise.race([p5, p6, p7])
+.then((value) => {
+    console.log(value);
+})
+.catch((err) => {
+    console.log('Caught: '+err);
+})
